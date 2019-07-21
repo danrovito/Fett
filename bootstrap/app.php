17ebfd680b103a2d851a5dc86aaa8ab2a94ae30d
Dan Rovito <danrovito@gmail.com>
2019-07-21 00:04:27 -0400

    added the controller and routes

---
+++ bootstrap/app.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
+
+$app = new Slim\App([
+    'settings' => [
+        'displayErrorDetails' => getenv('APP_DEBUG') === 'true',
+        'app' => [
+            'name' => getenv('APP_NAME'),
+        ],
+    ],
+]);
+
+$container = $app->getContainer();
+
+require_once __DIR__ . '/../routes/web.php';
